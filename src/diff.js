055503bbc9d97e275b3933070bdff213ca65c9da
vancomm <vanushnick@gmail.com>
2021-07-29 01:30:33 +0300

    Added basic CLI program

--- src/diff.js
+++ src/diff.js
@@ -1,1 +1,8 @@
-export default () => {};
+import program from 'commander';
+
+export default () => {
+  program
+    .description('Compares two configuration files and shows a difference. ')
+    .version('0.0.1');
+  program.parse();
+};
