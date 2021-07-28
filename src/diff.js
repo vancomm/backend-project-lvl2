0ef1db8084927a3c1d0cad2f17304873f8247a05
vancomm <vanushnick@gmail.com>
2021-07-29 01:42:33 +0300

    Added info on arguments in help message

--- src/diff.js
+++ src/diff.js
@@ -3,6 +3,8 @@ import program from 'commander';
 export default () => {
   program
     .description('Compares two configuration files and shows a difference. ')
-    .version('0.0.1');
+    .version('0.0.1')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filename1> <filename2>');
   program.parse();
 };
