4ba648ec46c767a2dcbc49e4d6078306e6480351
vancomm <vanushnick@gmail.com>
2021-08-03 16:51:30 +0300

    Modified cli app to pass format option to diff

--- src/cli.js
+++ src/cli.js
@@ -3,13 +3,13 @@ import diff from './diff.js';
 
 export default () => {
   program
-    .description('Compares two configuration files and shows a difference. ')
-    .version('0.0.1')
-    .option('-f, --format <type>', 'output format')
     .argument('<filename1>')
     .argument('<filename2>')
-    .action((filename1, filename2) => {
-      console.log(diff(filename1, filename2));
+    .description('Compares two configuration files and shows a difference. ')
+    .version('0.0.1')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .action((filename1, filename2, option) => {
+      console.log(diff(filename1, filename2, option));
     });
   program.parse();
 };
